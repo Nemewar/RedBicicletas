16b6d5e98ad0e36e14b633903f087407b576bcaa
nemewar <frank_gb_010@hotmail.com>
2022-09-02 23:22:27 -0500

    pagina principal

---
+++ public/javascripts/map.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo")
+var map = L.map('main_map').setView([-12.04591,-77.03047],18);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map); 
+
+
+L.marker([-12.045798,-77.029766]).addTo(map);
+L.marker([-12.046708,-77.030415]).addTo(map);
+L.marker([-12.046118,-77.031378]).addTo(map);
